96dd6bee01850909369a10c1e8c24fde712847b2
Alex <alexander.montesdeoca@davinci.edu.ar>
2020-10-19 22:33:48 -0300

    Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,268 @@
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&family=Vollkorn&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/cedf025736.js" crossorigin="anonymous"></script>
+    <title>Vie Nature Market</title>
+
+  <!-- Favicons -->
+  <link href="img/favicon.ico" rel="icon">
+  <link href="img/favicon.png" rel="favicon-icon">
+</head>
+
+<body class="fondo-portada">
+
+    <!-- Barra navegacion -->
+
+    <header>
+        <nav class="container-fluid navbar navbar-icon-top navbar-expand-lg navbar-dark fondonav fixed-top ">
+            <a id="marca" class="navbar-brand" href="index.php"><img class="img-fluid logos logo" src="img/logo.jpeg" alt="logo Vie"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php"><i class="fas fa-home"> Inicio </i><span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="productos.php"><i class="fas fa-box-open"> Productos </i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contacto.php"><i class="fas fa-address-book"> Contacto </i></a>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+    </header>
+   
+    <main>
+    	<section class="container">
+
+        <!-- identidad sitio -->
+
+        <div class="container col-lg-9 col-md-6 mb-4 justify-content-center text-center pt-5">
+            <img class="pt-5 img-fluid logomain" src="img/main.png" alt="logo Vie" style="  height: 250px;
+      width: 220px;
+ border-radius: 800spx;
+">
+           
+
+            <blockquote class="blockquote">
+                <p class="mb-0 text-light">"Tienda de productos naturales donde tu salud viene en primer lugar"</p>
+              
+            </blockquote>
+        </div>
+    </section>
+
+<div class="row">
+<div class="col-lg-12">
+	
+</div>
+</div>
+
+</div>
+
+
+<section>
+   
+
+        <div id="aside" class="container mt-5">
+
+            <div class="col-lg-10 m-auto">
+            	<blockquote class="blockquote">
+                <H2 class="mb-2 text-light">Destacados</H2>
+              
+            </blockquote>
+                <!--Productos destacados -->
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card">
+                       
+                            <a href=""><img src="img/pan.png" class="card-img-top text-center"></a>
+                            <div class="oferta badge badge-danger destacado">OFERTA</div>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href="">Pan La Zaranda</a>
+                                </h4>
+                                <span style="text-decoration:line-through;" class="badge badge-dark destacado"> $250</span>   <span>$149</span>
+                                <p class="card-text">Deliciosos panes de La Zaranda, nutricion con sabor Fuente de fibras 0% Grasas trans ...</p>
+                            </div>
+                       
+                        </div>
+                    </div>
+
+
+
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card">
+                       
+                            <a href=""><img class="card-img-top" src="img/barritasintegra.jpg" alt="">    <div class="oferta badge badge-danger destacado">OFERTA</div>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href=""> Barrita Integra</a>
+                                </h4>
+                                <span style="text-decoration:line-through;" class="badge badge-dark destacado">$100</span> <span>$60</span>
+                                <p class="card-text">Barras Integra
+                                Alimentacion Saludable
+                            Alimentos complejos que nutren y nos cuidan...</p>
+                            </div>
+                         </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card">
+                       
+                            <a href=""><img class="card-img-top" src="img/granolaintegral.jpg " alt="">    <div class="oferta badge badge-danger destacado">OFERTA</div>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href="detallesProducto.php?id=5">Granola Integra</a>
+                                </h4>
+                                <span style="text-decoration:line-through;" class="badge badge-dark destacado">$355</span> <span>$250</span>
+                                <p class="card-text">240 Grs Granola Integra Alimentacion Saludable
+                                              Alimentos complejos que nutren y nos cuidan...</p>
+                            </div>
+                           
+                        </div>
+                    </div>
+                  
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card">
+                       
+                            <a href=""><img class="card-img-top" src="img/yerbamatepampaorganic.jpg" alt="">    <div class="oferta badge badge-danger destacado">OFERTA</div>
+                            
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href="detallesProducto.php?id=8">Yerba Mate Pampa Organica</a>
+                                </h4>
+                               <span style="text-decoration:line-through;" class="badge badge-dark destacado">$400</span> <span>$325</span>
+                                <p class="card-text">Yerba Mate ORGÀNICA PAMPA, es producida siguiendo los principios de una agricultura sustentabl...</p>
+                            </div>
+                           
+                        </div>
+                    </div>
+                     <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card ">
+                       
+                            <a href=""><img class="card-img-top" src="img/rincon.png" alt="">    <div class="oferta badge badge-danger destacado">OFERTA</div>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href="detallesProducto.php?id=13">Un Rincon Vegano Dulces</a>
+                                </h4>
+                                    <span style="text-decoration:line-through;" class="badge badge-dark destacado">$139</span> <span>$80</span>
+                                <p class="card-text">SIN DERIVADOS DE ANIMALES Proba estas deliciosas galletas dulces en sus diferentes sabores:Pepas, Bananitas...</p>
+                            </div>
+                           
+                        </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card ">
+                       
+                            <a href=""><img class="card-img-top" src="img/harinadeavena.jpg" alt="">    <div class="oferta badge badge-danger destacado">OFERTA</div>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href="detallesProducto.php?id=13">Harina de Avena</a>
+                                </h4>
+                                    <span style="text-decoration:line-through;" class="badge badge-dark destacado">$220 </span> <span>$150</span>
+
+                                <p class="card-text"> Es una gran fuente de fibra pero tiene otros muchos beneficios: Ayuda a disminuir los niveles de colesterol en la sangre ...</p>
+                            </div>
+                           
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- fin productos destacados -->
+</main>
+<footer>
+	 <div class="p-3 mb-2 fondofooter text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm text-center">
+                    <h4>Programacion Web II</h4>
+                    <h5>Integrantes Grupo 6</h5>
+                    <p>Santiago Portillo<br> Adriana Vasquez  <br> Lucena Enya Beatriz.</p>
+                    
+                </div>
+
+                <div class="col">
+
+                    <h4 class="h3">Ayuda</h4>
+                    <ul class="footer-menu">
+                    <li class="footer-menu-item">Cómo comprar</li>	
+                    <li class="footer-menu-item">Medio de pago</li>	
+                    <li class="footer-menu-item"> Politica de cambio</li>	
+                 	
+
+
+                    </ul>
+                  
+                   
+                </div>
+
+                <div class="col-sm text-center">
+                    <h6>Seguinos en las redes sociales</h6>
+                        <ul class="justify-content-center list-group list-group-horizontal redes ">
+
+                            <li class="list-group-item fondofooter border-0">
+                                <a href="https://www.facebook.com/" target="new">
+                                    <i class="fab fa-facebook-f"></i>
+
+                                </a>
+                            </li>
+                            <li class="list-group-item fondofooter border-0">
+                                <a href="https://www.twitter.com/" target="new">
+                                    <i class="fab fa-twitter"></i>
+
+                                </a>
+                            </li>
+                            <li class="list-group-item fondofooter border-0">
+                                <a href="https://www.instagram.com/" target="new">
+                                    <i class="fab fa-instagram"></i>
+
+                                </a>
+                            </li>
+                            <li class="list-group-item fondofooter border-0">
+                                <a href="https://www.youtube.com.ar/" target="new">
+                                    <i class="fab fa-youtube"></i>
+                                </a>
+                            </li>
+                        </ul>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="col-sm text-center">
+   
+        <span>Medios de pago</span>	
+     
+  <span><img class="mediopago" src="img/medio.jpg"> </span> 	
+        </div>
+        <div class="col">
+        	
+
+
+        </div>
+
+      <span class="copy">&copy; Copyright 2020 Escuela de Arte Multimedial Da Vinci - Segundo Cuatrimestre</span>  
+    </div>
+
+</footer>
+    </body>
+    </html>
